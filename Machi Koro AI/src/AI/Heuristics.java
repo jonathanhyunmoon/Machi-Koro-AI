cee91c219df26d6d92ed2ca44b646b9b37d9cc5b
Jason Jung <jasojung17@gmail.com>
2019-04-26 14:43:27 -0400

    more value stuff

--- Machi Koro AI/src/AI/Heuristics.java
+++ Machi Koro AI/src/AI/Heuristics.java
@@ -17,11 +17,10 @@ public class Heuristics {
 	 */
 	public static int valueOneD(State s, Player p, Establishment c) {
 		LinkedList<Integer> actnums = c.get_activation_numbers();
-		if (actnums.get(0) > 6) return 0;
 		
-		int income = c.
-		// possible optimization in the future: hashmap with all expvals
-		// HashMap<LinkedList<Integer>,Integer> ExpVals = 
+		if (c.get_industry() == "Primary") {
+			int expval = expVal(actnums,c.)
+		}
 		
 		return 0;
 	}
@@ -42,5 +41,25 @@ public class Heuristics {
 	
 	public static int valueAvg(State s, Player p, Establishment c) {
 		return 0;
+	}
+	
+	/*
+	 * Returns the "non-dynamic" expected value of a linkedlist of
+	 * activation numbers. Accounts for number of dice.
+	 */
+	public static float expVal(LinkedList<Integer> actnums, int income, int numdice) {
+		if (numdice == 1) { // one die
+			if (actnums.get(0) > 6) return 0;
+			if (actnums.size() == 1) return (float) income * (float) 1 / 6;
+			return (float) income * (float) 2 / 6;
+		}
+		// two dice
+		else if (actnums.get(0) == 1) return 0;
+		float probtotal = 0;
+		for (Integer actnum : actnums) {
+			int dist = Math.abs(7-actnum);
+			probtotal += (float) (6-dist) / 36;
+		}
+		return probtotal * income;
 	}
 }
